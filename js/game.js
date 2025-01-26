ef3e4d00f9258c8b64520a0fbb75a7925ff54025
Bret Hudson <bret@brethudson.com>
2025-01-25 21:39:43 -0800

    Added backgrounds

--- js/game.js
+++ js/game.js
@@ -47,6 +47,13 @@ const ASSETS = {
 	BG2_PNG: 'bg2.png',
 	FLOORS_PNG: 'floors.png',
 
+	// backgrounds
+	BG_SUNSET: 'bg_0_sunset.png', // static
+	BG_CLOUD: 'bg_1_clouds.png', // static
+	BG_PYRAMIDS: 'pyramids.png', // parallax
+	BG_SKYSCRAPERS: 'parallax_parts_skyscrapers.png', // parallax
+	BG_FG: 'parallax_parts_fg.png', // parallax
+
 	// menu
 	LOGO: 'logo.png',
 };
@@ -468,6 +475,58 @@ class CameraManager extends Entity {
 	}
 }
 
+const levelWidth = 320;
+
+const buildingW = 960 / 6;
+
+let buildingIndices;
+{
+	const random = new Random(6465373);
+	buildingIndices = Array.from({ length: 100 }, (_, i) => {
+		return i % 5;
+	});
+}
+
+class Buildings extends Entity {
+	constructor(xOffset) {
+		super(xOffset * buildingW, 0);
+
+		this.graphic = new AnimatedSprite(
+			assetManager.sprites.get(ASSETS.BG_SKYSCRAPERS),
+			buildingW,
+			540,
+		);
+		this.depth = -1;
+		this.graphic.centerOO();
+
+		this.graphic.scrollX = 0.25;
+
+		this.graphic.add('0', [0], 100);
+		this.graphic.add('1', [1], 100);
+		this.graphic.add('2', [2], 100);
+		this.graphic.add('3', [3], 100);
+		this.graphic.add('4', [4], 100);
+
+		const fuck = buildingIndices[xOffset].toString();
+		console.log(fuck);
+		this.graphic.play(fuck);
+		this.graphic.update();
+	}
+
+	update() {
+		const camera = this.scene.camera;
+		const scrollX = this.graphic.scrollX;
+		const w = buildingW / scrollX;
+		if (this.x / this.graphic.scrollX < camera.x - buildingW * scrollX) {
+			console.log('left');
+			this.x += buildingW * 5;
+		}
+		if (this.x / this.graphic.scrollX > camera.x + buildingW * 4) {
+			// this.x -= buildingW * 5;
+		}
+	}
+}
+
 class Level extends Scene {
 	constructor(engine) {
 		super(engine);
@@ -483,22 +542,63 @@ class Level extends Scene {
 		const bg2 = new Background(ASSETS.BG2_PNG, canvasSize.y, assetManager);
 		const bg = new Background(ASSETS.BG_PNG, canvasSize.y, assetManager);
 		const tiles = new Tiles(assetManager);
-		[bg, bg2, tiles, p, cameraManager].forEach((e) => {
+
+		const entities = [
+			ASSETS.BG_SUNSET,
+			ASSETS.BG_CLOUD,
+			ASSETS.BG_PYRAMIDS,
+			// ASSETS.BG_SKYSCRAPERS,
+			// ASSETS.BG_FG,
+		]
+			.map((asset) => assetManager.sprites.get(asset))
+			.map((sprite) => {
+				const entity = new Entity(
+					engine.canvas.width >> 1,
+					(engine.canvas.height >> 1) - 80,
+				);
+				entity.graphic = new Sprite(sprite);
+				entity.graphic.scale = 0.5;
+				entity.graphic.centerOO();
+				console.log(entity.graphic);
+				return entity;
+			});
+
+		entities[0].graphic.scrollX = 0;
+		entities[1].graphic.scrollX = 0;
+		entities[2].graphic.scrollX = 0.025;
+		// entities[3].graphic.scrollX = 0.25;
+
+		for (let i = 0; i < 5; ++i) {
+			const buildings = new Buildings(i);
+			this.addEntity(buildings);
+			this.addRenderable(buildings);
+		}
+
+		// 0, 1, pyramids, sky, fg
+
+		[
+			...entities,
+			// bg,
+			// bg2,
+			// tiles,
+			p,
+			cameraManager,
+		].forEach((e) => {
 			this.addEntity(e);
 			this.addRenderable(e);
 		});
 
-		const random = new Random(settings.seed);
-		for (var i = 0; i < 3; i++) {
-			const e = new Grimey(
-				random.float(1000),
-				game.canvas.height - random.float(200),
-				assetManager,
-			);
+		// const random = new Random(settings.seed);
+		// for (var i = 0; i < 3; i++) {
+		// 	const e = new Grimey(
+		// 		random.float(1000),
+		// 		game.canvas.height - random.float(200),
+		// 		assetManager,
+		// 	);
 
-			this.addEntity(e);
-			this.addRenderable(e);
-		}
+		// 	this.addEntity(e);
+		// 	this.addRenderable(e);
+		// }
 	}
 
 	update(input) {
